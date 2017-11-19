5003866a4b413c698c96f8f613ae97e8c9480ca8
Kroganman <31574752+Kroganman@users.noreply.github.com>
2017-11-19 18:27:12 +0300

    Create calendar.c

---
+++ calendar.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b,c,d,m,y,x;
+   printf("Enter the date in the format: day month year\n");
+   scanf("%d %d %d",&d,&m,&y);
+   a=(14-m)/12;
+   b=y-a;
+   c=m+12*a-2;
+   x=(7000+d+b+b/4-b/100+b/400+(31*c)/12) % 7;
+   switch(x)
+   {
+   case 0:
+      printf("Sunday");
+      break;
+   case 1:
+      printf("Monday");
+      break;
+   case 2:
+      printf("Tuesday");
+      break;
+   case 3:
+      printf("Wednesday");
+      break;
+   case 4:
+      printf("Thursday");
+      break;
+   case 5:
+      printf("Friday");
+      break;
+   default:
+      printf("Saturday");
+      
+   }
+   return 0;
+}
